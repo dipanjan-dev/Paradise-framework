70b9117d4c359be4f26ebd759e921c48e7535e98
Dipanjan Mukherjee <67435621+dipanjan-dev@users.noreply.github.com>
2021-09-14 21:42:32 +0530

    New Version 1.00

---
+++ javascript/main.js
@@ -0,0 +1,64 @@
+console.log("Beyond your imaginations");
+
+
+// Get the modal
+var modal = document.getElementById("modal-body");
+
+// Get the button that opens the modal
+var btn = document.getElementById("modal-btn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+function removealert(){
+  var myobj = document.getElementById("alert");
+  myobj.remove();
+}
+
+
+// scroll-indicate
+
+function scroll_indicate(){
+
+    window.onscroll = function() {myFunction()};
+
+    function myFunction() {
+      var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+      var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+      var scrolled = (winScroll / height) * 100;
+      document.getElementById("scroll-bar").style.width = scrolled + "%";
+  };
+};
